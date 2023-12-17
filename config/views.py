1d7d22122164d4bb4f9da8f30e960a71f12a8a3d
FlynntKnapp <bruce.stull@gmail.com>
2023-12-17 06:03:41 -0500

    In progress.

--- config/views.py
+++ config/views.py
@@ -6,6 +6,7 @@ def messages_view(request):
     """
     A view that displays a message.
     """
-    messages.info(request, "This is the first custom message!")
-    messages.info(request, "This is the second custom message!")
+    messages.info(request, "This is the first custom message!", extra_tags="info")
+    messages.info(request, "This is the second custom message!", extra_tags="success")
+    messages.info(request, "This is the third custom message!", extra_tags="warning")
     return render(request, "index.html")
